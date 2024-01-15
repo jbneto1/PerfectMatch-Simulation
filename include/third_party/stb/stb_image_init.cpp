943a21c9faf0cfcc6deb2de526e97b31477d8309
BraunPC <jabraunneto1@gmail.com>
2024-01-15 21:11:09 +0000

    restructuring library files.

--- include/third_party/stb/stb_image_init.cpp
+++ include/third_party/stb/stb_image_init.cpp
@@ -3,6 +3,6 @@
 //
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "../../include/third_party/stb/stb_image.h"
+#include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../../include/third_party/stb/stb_image_write.h"
+#include "stb_image_write.h"
